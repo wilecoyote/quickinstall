d01bf2450c8d2d0d62f989d84d6c0146bc81722e
Andreas Fischer <bantu@phpbb.de>
2012-11-20 18:13:28 +0100

    [intl-de] Import of German (Casual Honorifics) / Deutsch (Du)

---
+++ language/de/info_acp_qi.php
@@ -0,0 +1,33 @@
+<?php
+/**
+*
+* info_acp_qi [Deutsch — Du]
+*
+* @package language
+* @version $Id: info_acp_qi.php 549 2011-02-07 19:37:05Z philippk $
+* @copyright (c) 2008 evil3
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+* Deutsche Übersetzung durch die Übersetzer-Gruppe von phpBB.de:
+* siehe docs/AUTHORS und http://www.phpbb.de/go/ubersetzerteam
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LOG_INSTALL_INSTALLED_QI'	=> '<strong>Installiert durch phpBB Quickinstall %s</strong>',
+));
+
+?>
