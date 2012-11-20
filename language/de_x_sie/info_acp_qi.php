f3a3abae58c98dda11269c9f866e4733896464c3
Andreas Fischer <bantu@phpbb.de>
2012-11-20 18:13:58 +0100

    [intl-de] Import of German (Formal Honorifics) / Deutsch (Sie)

---
+++ language/de_x_sie/info_acp_qi.php
@@ -0,0 +1,33 @@
+<?php
+/**
+*
+* info_acp_qi [Deutsch — Sie]
+*
+* @package language
+* @version $Id: info_acp_qi.php 559 2011-03-19 19:14:26Z philippk $
+* @copyright (c) 2008 evil3
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+* Deutsche Übersetzung durch die Übersetzer-Gruppe von phpBB.de:
+* siehe docs/AUTHORS und http://www.phpbb.de/go/ubersetzerteam
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LOG_INSTALL_INSTALLED_QI'	=> '<strong>Installiert durch phpBB Quickinstall %s</strong>',
+));
+
+?>
---
+++ language/de_x_sie/phpbb.php
@@ -0,0 +1,93 @@
+<?php
+/**
+*
+* phpbb [Deutsch — Sie]
+*
+* @package quickinstall
+* @version $Id: phpbb.php 559 2011-03-19 19:14:26Z philippk $
+* @copyright (c) 2007, 2008 eviL3
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+* Deutsche Übersetzung durch die Übersetzer-Gruppe von phpBB.de:
+* siehe docs/AUTHORS und http://www.phpbb.de/go/ubersetzerteam
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_QUICKINSTALL'))
+{
+	exit;
+}
+
+/**
+* DO NOT CHANGE
+*/
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'TRANSLATION_INFO'	=> 'Deutsche Übersetzung durch <a href="http://www.phpbb.de/">phpBB.de</a>',
+	'DIRECTION'			=> 'ltr',
+	'DATE_FORMAT'		=> '|d. M Y|',	// 01 Jan 2007
+	'USER_LANG'			=> 'de',
+	'USER_LANG_LONG'	=> 'Deutsch',
+
+	'datetime'			=> array(
+		'TODAY'		=> 'Heute',
+		'TOMORROW'	=> 'Morgen',
+		'YESTERDAY'	=> 'Gestern',
+
+		'Sunday'	=> 'Sonntag',
+		'Monday'	=> 'Montag',
+		'Tuesday'	=> 'Dienstag',
+		'Wednesday'	=> 'Mittwoch',
+		'Thursday'	=> 'Donnerstag',
+		'Friday'	=> 'Freitag',
+		'Saturday'	=> 'Samstag',
+
+		'Sun'		=> 'So',
+		'Mon'		=> 'Mo',
+		'Tue'		=> 'Di',
+		'Wed'		=> 'Mi',
+		'Thu'		=> 'Do',
+		'Fri'		=> 'Fr',
+		'Sat'		=> 'Sa',
+
+		'January'	=> 'Januar',
+		'February'	=> 'Februar',
+		'March'		=> 'März',
+		'April'		=> 'April',
+		'May'		=> 'Mai',
+		'June'		=> 'Juni',
+		'July'		=> 'Juli',
+		'August'	=> 'August',
+		'September' => 'September',
+		'October'	=> 'Oktober',
+		'November'	=> 'November',
+		'December'	=> 'Dezember',
+
+		'Jan'		=> 'Jan',
+		'Feb'		=> 'Feb',
+		'Mar'		=> 'Mär',
+		'Apr'		=> 'Apr',
+		'May_short'	=> 'Mai',	// Short representation of "May". May_short used because in English the short and long date are the same for May.
+		'Jun'		=> 'Jun',
+		'Jul'		=> 'Jul',
+		'Aug'		=> 'Aug',
+		'Sep'		=> 'Sep',
+		'Oct'		=> 'Okt',
+		'Nov'		=> 'Nov',
+		'Dec'		=> 'Dez',
+	),
+
+	// The default dateformat which will be used on new installs in this language
+	// Translators should change this if a the usual date format is different
+	'default_dateformat'	=> 'D j. M Y, H:i', // Mo 1. Jan 2007, 13:57
+
+));
+
+?>
